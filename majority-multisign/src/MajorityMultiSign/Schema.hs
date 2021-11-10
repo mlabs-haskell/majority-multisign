9b6246a80071897ffd6cf906945c04f6274d19e2
Mario Blažević <blamario@protonmail.com>
2021-11-10 09:09:00 -0500

    Added missing instances for MajorityMultiSignIdentifier

--- majority-multisign/src/MajorityMultiSign/Schema.hs
+++ majority-multisign/src/MajorityMultiSign/Schema.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 -- record-dot-preprocessor creates code that violates this warning, disable for this file
 {-# OPTIONS_GHC -Wno-incomplete-record-updates #-}
+-- disabling orphans warning to be able to write ToArgument instances
+{-# OPTIONS_GHC -Wno-orphans #-}
+-- core is not inlineable without this
 {-# OPTIONS_GHC -fno-specialise #-}
 
 module MajorityMultiSign.Schema (
@@ -18,17 +22,28 @@ module MajorityMultiSign.Schema (
 ) where
 
 import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson.Extras (encodeByteString)
+import Data.Functor.Foldable (Fix (..))
+import Data.OpenApi.Schema qualified as OpenApi
+import Data.Text qualified as Text
+import Data.Text.Encoding (decodeUtf8)
 import GHC.Generics (Generic)
 import Ledger.Crypto (PubKey)
 import Ledger.Typed.Scripts qualified as Scripts
 import Plutus.Contract (Endpoint, type (.\/))
 import Plutus.V1.Ledger.Api (PubKeyHash)
-import Plutus.V1.Ledger.Value (AssetClass)
+import Plutus.V1.Ledger.Value (AssetClass, CurrencySymbol (..), TokenName (..))
 import PlutusTx qualified
 import PlutusTx.NatRatio (NatRatio, ceiling, frac, fromNatural)
 import PlutusTx.Natural (Natural)
-import PlutusTx.Prelude hiding (Eq)
+import PlutusTx.Prelude hiding (decodeUtf8, Eq)
 import Prelude (Eq, Show)
+import Schema (
+  FormArgumentF (..),
+  FormSchema (..),
+  ToArgument (toArgument),
+  ToSchema (toSchema),
+ )
 
 {-# INLINEABLE signReq #-}
 
@@ -54,16 +69,37 @@ getMinSigners = ceilNatRatioToInt . (signReq *) . intToNatRatio . length
 data MajorityMultiSignIdentifier = MajorityMultiSignIdentifier
   { asset :: AssetClass
   }
-  deriving stock (Generic, Show)
-  deriving anyclass (FromJSON, ToJSON)
+  deriving stock (Eq, Generic, Show)
+  deriving anyclass (FromJSON, OpenApi.ToSchema, ToJSON)
 
 PlutusTx.unstableMakeIsData ''MajorityMultiSignIdentifier
 PlutusTx.makeLift ''MajorityMultiSignIdentifier
+
+instance ToSchema MajorityMultiSignIdentifier where
+  toSchema =
+    FormSchemaObject [ ("asset", FormSchemaMaybe (toSchema @AssetClass)) ]
+
+instance ToArgument MajorityMultiSignIdentifier where
+  toArgument MajorityMultiSignIdentifier {asset} =
+    Fix $ FormObjectF [ ("asset", toArgument (Just asset)) ]
 
 -- | Params to the majority multisign validator, as the asset class of the `MajorityMultiSignDatum`
 data MajorityMultiSignValidatorParams = MajorityMultiSignValidatorParams
   { asset :: AssetClass
   }
+
+instance ToArgument a => ToArgument (Maybe a) where
+  toArgument = Fix . FormMaybeF (toSchema @a) . fmap toArgument
+
+deriving anyclass instance ToArgument AssetClass
+
+instance ToArgument CurrencySymbol where
+  toArgument (CurrencySymbol builtinBS) =
+    Fix $ FormHexF $ Just $ Text.unpack $ encodeByteString $ fromBuiltin builtinBS
+
+instance ToArgument TokenName where
+  toArgument (TokenName builtinBS) =
+    Fix $ FormStringF $ Just $ Text.unpack $ decodeUtf8 $ fromBuiltin builtinBS
 
 PlutusTx.makeLift ''MajorityMultiSignValidatorParams
 
